ff390d573adc5dd2f7d3bc58158a6a787eb2d0c6
Maliniak, Michael <Michael.Maliniak@news.com.au>
2017-02-13 11:07:15 +1100

    Json stuff

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var mysql = require('mysql');
 
 var apiController = require('./controllers/apiController.js');
 var htmlController = require('./controllers/htmlController.js');
@@ -10,6 +11,33 @@ app.use('/dist', express.static(__dirname + '/dist'));
 
 app.set('view engine', 'ejs');
 
+app.use('/', function(req, res, next) {
+    console.log('Request Url:' + req.url);
+
+    /* MySQL */
+
+    var con = mysql.createConnection({
+            host: 'localhost',
+            user: 'test',
+            password: 'test',
+            database: 'addressbook'
+        });
+        
+    /* CUT and PASTE SQL CODE*/
+
+    con.query(
+        '', function(err, rows) {
+            if(err) throw err;
+            console.log(rows[0].Firstname);
+        }
+    );
+
+    next();
+});
+
+
+
+/* Route Controllers: */
 htmlController(app);
 apiController(app);
 
