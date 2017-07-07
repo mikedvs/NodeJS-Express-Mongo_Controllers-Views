aef167b001cbcb98d2567d7f932aed2d78f5b3e7
Maliniak, Michael <Michael.Maliniak@news.com.au>
2017-07-07 15:27:35 +1000

    updated mongo connection..still having issues

--- express.js
+++ express.js
@@ -4,6 +4,29 @@ var apiController = require('./controllers/apiController.js');
 var htmlController = require('./controllers/htmlController.js');
 var mongoose = require('mongoose');
 
+mongoose.Promise = require('q').Promise;
+
+/* 
+ * Mongoose by default sets the auto_reconnect option to true.
+ * We recommend setting socket options at both the server and replica set level.
+ * We recommend a 30 second connection timeout because it allows for 
+ * plenty of time in most operating environments.
+ */
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };       
+ 
+var mongodbUri = 'mongodb://mikedvs:whiskey2@ds017672.mlab.com:17672/addressbook';
+ 
+mongoose.connect(mongodbUri, options);
+var conn = mongoose.connection;             
+ 
+conn.on('error', console.error.bind(console, 'connection error:'));  
+ 
+conn.once('open', function() {
+  // Wait for the database connection to establish, then start the app.                         
+});
+
+
 var port = process.env.PORT || 3000;
 
 // HTML and API Controllers
@@ -13,8 +36,6 @@ apiController(app);
 app.use('/dist', express.static(__dirname + '/dist'));
 
 app.set('view engine', 'ejs');
-
-mongoose.connect('mongodb://mikedvs:whiskey2@ds017672.mlab.com:17672/addressbook');
 
 var Schema = mongoose.Schema;
 
